702bd37760b33af87aadd9c85ee7ca5797214644
tiaxter <jerrytiapalmiotto@gmail.com>
2022-11-03 16:22:12 +0100

    refactor: remade the bot using typegram typings to generate method dynamically

--- index.ts
+++ index.ts
@@ -1,43 +1,30 @@
-import {Ret, Opts} from "typegram";
+import {Ret, Opts, Telegram} from "typegram";
 import {req} from "./utils";
 
-export class Bungram {
+type Method = keyof Telegram;
+
+const availableMethods: Method[] = [
+    'getMe',
+    'sendMessage',
+    'sendLocation',
+    'editMessageLiveLocation',
+    'stopMessageLiveLocation',
+];
+
+type HasMethods = { [k in typeof availableMethods[number]]: (params?: Opts<k>) => Promise<Ret<k>> }
+
+export const Bungram = class Bungram {
     private readonly TOKEN;
     private readonly ENDPOINT;
 
     constructor(token: string) {
         this.TOKEN = token;
         this.ENDPOINT = `https://api.telegram.org/bot${this.TOKEN}`;
-    }
 
-    public getMe() : Promise<Ret<'getMe'>> {
-        return req(`${this.ENDPOINT}/getMe`);
+        availableMethods.forEach(method => {
+            this[method] = (params: Opts<typeof method>): Promise<Ret<typeof method>> => {
+                return req(`${this.ENDPOINT}/${method}`, params);
+            };
+        });
     }
-
-    public logOut() : Promise<Ret<'logOut'>> {
-       return req(`${this.ENDPOINT}/logOut`);
-    }
-    
-    public close() : Promise<Ret<'close'>> {
-        return req(`${this.ENDPOINT}/close`);
-    }
-
-    public sendMessage(params: Opts<'sendMessage'>) : Promise<Ret<'sendMessage'>> {
-       return req(`${this.ENDPOINT}/sendMessage`, params);
-    }
-
-    public forwardMessage(params: Opts<'forwardMessage'>) : Promise<Ret<'forwardMessage'>> {
-       return req(`${this.ENDPOINT}/forwardMessage`, params);
-    }
-
-    public copyMessage(params: Opts<'copyMessage'>) : Promise<Ret<'copyMessage'>> {
-        return req(`${this.ENDPOINT}/copyMessage`, params);
-    }
-
-    // TODO: must implement methods: sendPhoto, sendAudio, sendDocument, sendVideo, sendAnimation,
-    //                               sendVoice, sendVideoNote, sendMediaGroup
-
-    public sendLocation(params: Opts<'sendLocation'>) : Promise<Ret<'sendLocation'>> {
-        return req(`${this.ENDPOINT}/sendLocation`, params);
-    }
-}
+} as unknown as { new(token: string): HasMethods };
